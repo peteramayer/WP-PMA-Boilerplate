091c3508e665c5ff8b045f9bdee1010f19a807e5
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-07-13 15:52:12 -0500

    add key email

--- Github-Web-Deploy/config.php
+++ Github-Web-Deploy/config.php
@@ -12,6 +12,7 @@ $repo_clone_path = 'git@github.com:peteramayer/WP-PMA-Boilerplate.git';
 #REPO BRANCH NAME
 #May look like: 'master'
 $repo_branch_name = 'master';
+$git_key_email = 'server@peteramayer.com';
 
 if ( !$ssh_key_path || !$ssh_pubkey_name || !$ssh_key_name || !$repo_clone_path || !$repo_branch_name ) {
 	echo "CONFIGURE YOUR REPO INSIDE config.php FIRST!";
--- Github-Web-Deploy/setup.php
+++ Github-Web-Deploy/setup.php
@@ -4,7 +4,7 @@ echo "<pre>";
 
 $current_key = shell_exec('cat '.$ssh_key_path.$ssh_pubkey_name);
 if ( !$current_key ) {
-	$output = shell_exec( 'cd '.$ssh_key_path.' && ssh-keygen -t rsa -C "server@peteramayer.com" -N "" -f '.$ssh_key_name.' 2>&1' );
+	$output = shell_exec( 'cd '.$ssh_key_path.' && ssh-keygen -t rsa -C "'.$git_key_email.'" -N "" -f '.$ssh_key_name.' 2>&1' );
 
 	$output .= "\n Refresh to see the public key for use in GitHub.";
 } else {
