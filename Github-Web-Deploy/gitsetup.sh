87af501c6302ac9b1d58dd45adf6409235ffbb90
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-07-13 13:43:05 -0500

    add vagrant

--- Github-Web-Deploy/gitsetup.sh
+++ Github-Web-Deploy/gitsetup.sh
@@ -1,3 +1,3 @@
-git clone #https://peteramayer@github.com/peteramayer/repo.git #gitreponame
 cd ~/.ssh
-ssh-keygen -t rsa -C "server@websitenamehere.com" -N "" -f id_rsa
+ssh-keygen -t rsa -C "server@peteramayer.com" -N "" -f id_rsa
+#git clone git@github.com:peteramayer/WP-PMA-Boilerplate.git #gitreponame
